b7fda313e391f4d7f9f4ce546f0e116c756e5410
takami <ksuper_storeaaa@yahoo.co.jp>
2021-04-20 18:13:02 +0900

    fixed Variable name, use map method, use shift method

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -14,42 +14,40 @@ scores.each do |x|
 end
 
 frames = []
-shots.each_slice(2) do |s|
-  frames << s
-end
+shots.each_slice(2).map { |x| frames << x }
 
 if frames.length == 11 && frames[9][0] != 10
-  frames[9] << frames[10][0]
+  frames[9] << frames[10].shift
   frames.delete_at(10)
 end
 
 if frames.length == 11 && frames[9][0] == 10
   frames[9].delete_at(-1)
-  frames[9] << frames[10][0]
-  frames[9] << frames[10][1]
+  frames[9] << frames[10].shift
+  frames[9] << frames[10].shift
   frames.delete_at(10)
 end
 
 if frames.length == 12
   frames[9].delete_at(-1)
-  frames[9] << frames[10][0]
-  frames[9] << frames[11][0]
+  frames[9] << frames[10].shift
+  frames[9] << frames[11].shift
   2.times { frames.delete_at(10) }
 end
 
-last_score = 0
+sum_score = 0
 frames.each.with_index(1) do |frame, i|
   if i <= 8 && frame[0] == 10 && frames[i][0] == 10 # 2連続ストライクの場合の処理
-    last_score += frames[i][0] + frames[i + 1][0]
+    sum_score += frames[i][0] + frames[i + 1][0]
   elsif i <= 8 && frame[0] == 10 # ストライク単発の処理
-    last_score += frames[i].sum
+    sum_score += frames[i].sum
   elsif i <= 9 && frame.sum == 10 && frame[0] != 10 # スペアの処理
-    last_score += frames[i][0]
+    sum_score += frames[i][0]
   elsif i == 9 && frame[0] == 10 # 9投目がストライクの場合の処理
-    last_score += frames[i][0] + frames[i][1]
+    sum_score += frames[i][0] + frames[i][1]
   end
 
-  last_score += frame.sum
+  sum_score += frame.sum
 end
 
-puts last_score
+puts sum_score
