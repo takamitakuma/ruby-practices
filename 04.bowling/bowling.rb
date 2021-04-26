01208ed883a173491fb33b238ffded16ed2c5644
takami <ksuper_storeaaa@yahoo.co.jp>
2021-04-26 21:58:11 +0900

    fix conditional branch

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -17,34 +17,32 @@ frames = []
 shots.each_slice(2).map { |x| frames << x }
 
 if frames.length == 11 && frames[9][0] != 10
-  frames[9] << frames[10].shift
-  frames.delete_at(10)
+  frames[9] << frames[10][0]
+  frames.pop
 end
 
 if frames.length == 11 && frames[9][0] == 10
   frames[9].delete_at(-1)
-  frames[9] << frames[10].shift
-  frames[9] << frames[10].shift
-  frames.delete_at(10)
+  frames[9] << frames[10][0]
+  frames[9] << frames[10][0]
+  frames.pop
 end
 
 if frames.length == 12
   frames[9].delete_at(-1)
-  frames[9] << frames[10].shift
-  frames[9] << frames[11].shift
-  2.times { frames.delete_at(10) }
+  frames[9] << frames[10][0]
+  frames[9] << frames[11][0]
+  2.times { frames.pop }
 end
 
 sum_score = 0
 frames.each.with_index(1) do |frame, i|
   if i <= 8 && frame[0] == 10 && frames[i][0] == 10 # 2連続ストライクの場合の処理
     sum_score += frames[i][0] + frames[i + 1][0]
-  elsif i <= 8 && frame[0] == 10 # ストライク単発の処理
-    sum_score += frames[i].sum
-  elsif i <= 9 && frame.sum == 10 && frame[0] != 10 # スペアの処理
-    sum_score += frames[i][0]
-  elsif i == 9 && frame[0] == 10 # 9投目がストライクの場合の処理
-    sum_score += frames[i][0] + frames[i][1]
+  elsif frame[0] == 10 # ストライク単発の処理
+    sum_score += frames[i][0] + frames[i][1] if i <= 9
+  elsif frame.sum == 10 # スペアの処理
+    sum_score += frames[i][0] if i <= 9
   end
 
   sum_score += frame.sum
