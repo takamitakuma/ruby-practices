f344346b7c43367a9a7a6f347b1fd53a348494d5
takami <ksuper_storeaaa@yahoo.co.jp>
2021-06-17 05:57:20 +0900

    fix variable name, iterative processing, and some other expression

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -4,58 +4,55 @@
 require 'optparse'
 
 def main(options)
-  file_names = ARGV
-  file_datas = []
-
   if ARGV[0].nil? # 標準入力から受け取る際の処理
     stdin_value = $stdin.read
-    options['l'] ? stdin_option_l(stdin_value, file_datas) : stdin_no_option(stdin_value, file_datas)
+    options['l'] ? stdin_option_l(stdin_value) : stdin_no_option(stdin_value)
   else
-    options['l'] ? option_l(file_names, file_datas) : no_option(file_names, file_datas)
+    file_names = ARGV
+    options['l'] ? option_l(file_names) : no_option(file_names)
   end
-  puts file_datas
 end
 
-def option_l(file_names, file_datas)
+def option_l(file_names)
   total_row = 0
-  file_names.each do |f|
-    io_file = File.open(f).read
-    row_count = io_file.count("\n")
+  file_names.each do |file_name|
+    file_content = File.open(file_name) { |file| file.read }
+    row_count = file_content.count("\n")
     total_row += row_count if file_names.size > 1
-    file_datas << "#{row_count.to_s.rjust(8)} #{f}"
+    puts "#{row_count.to_s.rjust(8)} #{file_name}"
   end
-  file_datas << "#{total_row.to_s.rjust(8)} total" if file_names.size > 1
+  puts "#{total_row.to_s.rjust(8)} total" if file_names.size > 1
 end
 
-def no_option(file_names, file_datas)
+def no_option(file_names)
   total_row = 0
   total_word = 0
-  total_bite = 0
-  file_names.each do |f|
-    io_file = File.open(f).read
-    row_count = io_file.count("\n")
-    word_count = io_file.split(/\s+/).count
-    bite_count = io_file.bytesize
+  total_byte = 0
+  file_names.each do |file_name|
+    file_content = File.open(file_name) { |file| file.read }
+    row_count = file_content.count("\n")
+    word_count = file_content.split(/\s+/).count
+    byte_count = file_content.bytesize
     if file_names.size > 1
       total_row += row_count
       total_word += word_count
-      total_bite += bite_count
+      total_byte += byte_count
     end
-    file_datas << "#{row_count.to_s.rjust(8)}#{word_count.to_s.rjust(8)}#{bite_count.to_s.rjust(8)} #{f}"
+    puts "#{[row_count, word_count, byte_count].map { |value| value.to_s.rjust(8) }.join} #{file_name}"
   end
-  file_datas << "#{total_row.to_s.rjust(8)}#{total_word.to_s.rjust(8)}#{total_bite.to_s.rjust(8)} total" if file_names.size > 1
+  puts "#{[total_row, total_word, total_byte].map { |value| value.to_s.rjust(8) }.join} total" if file_names.size > 1
 end
 
-def stdin_no_option(stdin_value, file_datas)
+def stdin_no_option(stdin_value)
   row_count = stdin_value.count("\n")
   word_count = stdin_value.split(/\s+/).count
-  bite_count = stdin_value.bytesize
-  file_datas << "#{row_count.to_s.rjust(8)}#{word_count.to_s.rjust(8)}#{bite_count.to_s.rjust(8)}"
+  byte_count = stdin_value.bytesize
+  puts [row_count, word_count, byte_count].map { |value| value.to_s.rjust(8) }.join
 end
 
-def stdin_option_l(stdin_value, file_datas)
+def stdin_option_l(stdin_value)
   row_count = stdin_value.count("\n")
-  file_datas << row_count.to_s.rjust(8)
+  puts row_count.to_s.rjust(8)
 end
 
 options = ARGV.getopts('l')
